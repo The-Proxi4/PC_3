8cf1f7b7917dfd34abe5dc6667328e969dacc6cb
Renzo Huaman <huaman22hm@gmail.com>
2025-05-23 23:00:17 -0500

    Proyecto NoticiasEnriquecidas: versión lista para deploy en Render luego de usar estilo

--- Services/JsonPlaceholderService.cs
+++ Services/JsonPlaceholderService.cs
@@ -6,21 +6,25 @@ using System.Text.Json;
 
 namespace NoticiasEnriquecidas.Services
 {
+    //aqui se define la interfaz IJsonPlaceholderService
     public class JsonPlaceholderService : IJsonPlaceholderService
     {
+        //aqui se define la variable _httpClient
         private readonly HttpClient _httpClient;
+        //aqui se define la variable BaseUrl
         private const string BaseUrl = "https://jsonplaceholder.typicode.com/";
-
+        //aqui se define la variable _jsonOptions
         private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        //aqui se define la variable _jsonOptions
         {
             PropertyNameCaseInsensitive = true
         };
-
+        //aqui se define el constructor JsonPlaceholderService
         public JsonPlaceholderService(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
-
+        //aqui se define el metodo GetPostsAsync
         public async Task<List<Post>> GetPostsAsync()
         {
             var response = await _httpClient.GetAsync($"{BaseUrl}posts");
@@ -28,7 +32,7 @@ namespace NoticiasEnriquecidas.Services
             var json = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<List<Post>>(json, _jsonOptions);
         }
-
+        //aqui se define el metodo GetPostByIdAsync
         public async Task<Post> GetPostByIdAsync(int postId)
         {
             var response = await _httpClient.GetAsync($"{BaseUrl}posts/{postId}");
@@ -36,7 +40,7 @@ namespace NoticiasEnriquecidas.Services
             var json = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<Post>(json, _jsonOptions);
         }
-
+        //aqui se define el metodo GetUserByIdAsync
         public async Task<User> GetUserByIdAsync(int userId)
         {
             var response = await _httpClient.GetAsync($"{BaseUrl}users/{userId}");
@@ -44,6 +48,7 @@ namespace NoticiasEnriquecidas.Services
             var json = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<User>(json, _jsonOptions);
         }
+        //aqui se define el metodo GetCommentsByPostIdAsync
 
         public async Task<List<Comment>> GetCommentsByPostIdAsync(int postId)
         {
